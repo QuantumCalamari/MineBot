bb81e46ca9be8359f809c059ecee73adf13d9116
Jon Collins <jonacollins3@gmail.com>
2017-01-09 16:45:05 +0000

    pretty close to working

---
+++ src/minesweeper/mainMine.java
@@ -0,0 +1,154 @@
+package minesweeper;
+
+import java.util.Random;
+import minesweeper.solver;
+
+public class mainMine {
+
+	public static int searchAdjacent(int x, int y, int[][] botBoard, int[][] gameBoard) {
+		
+		int r = 1;
+		
+		for (int sy = (y - 1); sy < (y + 2); sy++) {
+			for (int sx = (x - 1); sx < (x + 2); sx++) {
+				if (sx >= 0 && sx < 30 && sy >= 0 && sy < 16) {
+					if (sx == x && sy == y) {
+						
+					} else {
+						if (botBoard[sx][sy] != gameBoard[sx][sy]) {
+							botBoard[sx][sy] = gameBoard[sx][sy];
+							r = 0;
+						}
+						if (botBoard[sx][sy] == 0) {
+							
+							
+						}
+					}
+				}
+			}
+		}
+		
+		return r;
+	}
+	
+	public static void main(String[] args) {
+		
+		int[][] gameBoard = new int[30][16];
+		int bombs = 0;
+		Random rand = new Random();
+
+		int  xr, yr;
+		
+		while (bombs < 99) {
+			xr = rand.nextInt(30);
+			yr = rand.nextInt(16);
+
+			if (gameBoard[xr][yr] != 9) {
+				gameBoard[xr][yr] = 9;
+				bombs++;
+			}
+		}
+		
+		for (int y = 0; y < 16; y++) {
+			for (int x = 0; x < 30; x++) {
+				for (int searchy = y-1; searchy < (y+2); searchy++) {
+					for (int searchx = x-1; searchx < (x+2); searchx++) {
+						if ((searchx >= 0) && (searchx < 30) && (searchy >= 0) && (searchy < 16)) {
+							if ((gameBoard[searchx][searchy] == 9) && (gameBoard[x][y] != 9)) {
+								gameBoard[x][y]++;
+							}
+						}
+					}
+				}
+				if (gameBoard[x][y] == 0) {
+					System.out.print("- ");
+				} else {
+					System.out.print(gameBoard[x][y] + " ");
+				}
+			}
+			System.out.println();
+		}
+		
+		System.out.println("------------------------");
+		System.out.println("Time to solve!");
+		System.out.println("------------------------");
+		
+		//create a new bot
+		solver pivot = new solver();
+		
+		pivot.botBoard = new int[30][16];
+		
+		//make all boxes 0
+		//0 on gameboard means a blank square
+		//0 on botboard is unseen
+		for (int i = 0; i < 30; i++) {
+			for (int j = 0; j < 16; j++) {
+				pivot.botBoard[i][j] = 10;
+			}
+		}
+		
+		//select random point
+		xr = rand.nextInt(30);
+		yr = rand.nextInt(16);
+		
+		
+		//make sure the first click isn't a bomb
+		while (gameBoard[xr][yr] == 9) {
+			xr = rand.nextInt(30);
+			yr = rand.nextInt(16);
+		}
+		
+		if (gameBoard[xr][yr] != 9) {
+			pivot.botBoard[xr][yr] = gameBoard[xr][yr];
+			//System.out.println(botBoard[xr][yr]);
+			System.out.println("xr: " + xr);
+			System.out.println("yr: " + yr);
+		}
+
+		//search surrounding area
+		if (pivot.botBoard[xr][yr] == 0) {
+			searchAdjacent(xr, yr, pivot.botBoard, gameBoard);
+		}
+		
+		for (int i = 0; i < 30; i++) {
+			for (int j = 0; j < 16; j++) {
+				if (pivot.botBoard[i][j] == 0) {
+					if (searchAdjacent(i, j, pivot.botBoard, gameBoard) == 0) {
+						i = 0;
+						j = 0;
+					}
+				}
+			}
+		}
+		
+		for (int y = 0; y < 16; y++) {
+			for (int x = 0; x < 30; x++) {
+					System.out.print(pivot.botBoard[x][y] + " ");
+			}
+				System.out.println();
+		}
+		pivot.scanner();
+		pivot.clicker(gameBoard);
+		
+		System.out.println("------------------------");
+		System.out.println("Adjacent");
+		System.out.println("------------------------");
+			
+
+		
+		for (int y = 0; y < 16; y++) {
+			for (int x = 0; x < 30; x++) {
+				if (pivot.botBoard[x][y] == 10) {
+					System.out.print("- ");
+				} else {
+					System.out.print(pivot.adjP[x][y] + " ");
+				}
+			}
+				System.out.println();
+		}
+
+		
+	
+	}
+
+}
---
+++ src/minesweeper/solver.java
@@ -0,0 +1,132 @@
+package minesweeper;
+
+public class solver {
+
+	int[][] botBoard = new int[30][16];
+	
+	//this board gives the probability that the adjacent tiles are bombs
+	float[][] adjP = new float[30][16];
+	
+	//the number of unclicked boxes adjacent
+	int[][] adjC = new int[30][16];
+	
+	//this board gives the probability that the tile we're on should be clicked
+	float[][] clickP = new float[30][16];
+	
+	//threshold value
+	float a = 0.7f;
+	
+	public void scanner() {
+		//scan through the board and assign the adjacent probabilities
+		for (int y = 0; y < 16; y++) {
+			for (int x = 0; x < 30; x++) {
+				if (botBoard[x][y] != 10 && botBoard[x][y] != 0) {
+					
+					adjC[x][y] = 0;
+					
+					for (int sy = (y - 1); sy < (y + 2); sy++) {
+						for (int sx = (x - 1); sx < (x + 2); sx++) {
+							if (sx >= 0 && sx < 30 && sy >= 0 && sy < 16) {
+								//if (sx != x && sy != y) {
+									
+									//System.out.println("sx: " + sx + ", sy: " + sy);
+									if (botBoard[sx][sy] == 10) {
+										adjC[x][y]++;
+									}	
+								//}							
+							}
+						}
+					}
+					adjP[x][y] = ((float) botBoard[x][y]/((float) adjC[x][y]));
+				}
+			}
+		}
+		
+		//now go back through and assess the probability that each box should be clicked
+		for (int y = 0; y < 16; y++) {
+			for (int x = 0; x < 30; x++) {
+				
+				clickP[x][y] = 0;
+				
+				for (int sy = (y - 1); sy < (y + 2); sy++) {
+					for (int sx = (x - 1); sx < (x + 2); sx++) {
+						if (sx >= 0 && sx < 30 && sy >= 0 && sy < 16) {
+							if ((sx != x) || (sy != y)) {
+								clickP[x][y] = clickP[x][y] + adjP[sx][sy];
+							}
+							
+					}
+				}
+				//clickP[x][y] = clickP[x][y]/adjC[x][y];
+					
+			}
+		}
+	}
+		
+		System.out.println("------------------------");
+		System.out.println("ClickP");
+		System.out.println("------------------------");
+		
+		for (int y = 0; y < 16; y++) {
+			for (int x = 0; x < 30; x++) {
+				//if (botBoard[x][y] == 10) {
+				//	System.out.print("- ");
+				//} else {
+					System.out.print(clickP[x][y] + " ");
+				//}
+			}
+				System.out.println();
+		}
+	}
+	
+	public boolean clicker(int[][] gameBoard) {
+		
+		for (int y = 0; y < 16; y++) {
+			for (int x = 0; x < 30; x++) {
+			//	System.out.println("a: " + a);
+			//	System.out.println("click: " + clickP[x][y]);
+				if ((clickP[x][y] < a) && (clickP[x][y] != 0)) {
+					//System.out.println("x: " + x + ", y: " + y);
+					//if (gameBoard[x][y] == 10) {
+					//	return true;
+					//} else {
+					botBoard[x][y] = gameBoard[x][y];
+					//}
+			}
+		}
+		}
+		
+		System.out.println("------------------------");
+		System.out.println("Updated");
+		System.out.println("------------------------");
+		
+		for (int y = 0; y < 16; y++) {
+			for (int x = 0; x < 30; x++) {
+				//if (botBoard[x][y] == 10) {
+				//	System.out.print("- ");
+				//} else {
+					System.out.print(botBoard[x][y] + " ");
+				//}
+			}
+				System.out.println();
+		}
+		
+		System.out.println("------------------------");
+		System.out.println("ClickP");
+		System.out.println("------------------------");
+		
+		for (int y = 0; y < 16; y++) {
+			for (int x = 0; x < 30; x++) {
+				//if (botBoard[x][y] == 10) {
+				//	System.out.print("- ");
+				//} else {
+					System.out.print(clickP[x][y] + " ");
+				//}
+			}
+				System.out.println();
+		}
+		
+		return false;
+	}
+	
+}
